8bd4801d544d455658d6a90bf354e6d71aefbf0d
xtsulyts <531xtsulyts@gmail.com>
2025-08-18 22:12:49 -0300

    puntos 2 y 3 ok, agrego .gitignore no subo el archivo json que creamos al remoto, se crea solo en local.

--- tp_2/apiFetch.js
+++ tp_2/apiFetch.js
@@ -1,15 +1,41 @@
 const axios = require('axios');
-
+const fs = require('fs');
 const API_URL = 'https://fakestoreapi.com/products';
 
-async function productos() {
+/**
+ * 1-Recuperar la información de todos los productos (GET).
+ */
+// async function productos() {
+//   try {
+//     const response = await axios.get(API_URL);
+//     console.log('Todos los productos:', response.data);pro
+//     return response.data;
+//   } catch (error) {
+//     console.error('Error al obtener productos:', error.message);
+//   }
+// }
+
+// productos();
+
+/**
+ * 2-Recuperar la información de un número limitado de productos (GET).
+ */
+console.log("=====Recuperar la información de un número limitado de productos (GET)======")
+async function productosLimitados(limite, productosJSON) {
   try {
-    const response = await axios.get(API_URL);
-    console.log('Todos los productos:', response.data);
+    const response = await axios.get(`${API_URL}?limit=${limite}`);
+    console.log(`Primeros ${limite} productos:`, response.data);
+    /**
+     * 3-Persistir los datos de la consulta anterior en un archivo local JSON.
+     */
+    const productosJason = response.data;
+    fs.writeFileSync(productosJSON, JSON.stringify(productosJason, null, 2));
+    console.log(`Productos guardados en ${productosJSON}`);
     return response.data;
   } catch (error) {
-    console.error('Error al obtener productos:', error.message);
+    console.error('Error al obtener productos limitados:', error.message);
   }
 }
 
-productos();
+// Ejemplo: Obtenemos 5 productos
+productosLimitados(5, 'productosJSON.json');
